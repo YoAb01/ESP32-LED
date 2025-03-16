a225f9a577387518fa108826507726ce29b5eb02
YoAB01 <youssef.abl.learn.25@gmail.com>
2025-03-16 20:35:50 +0100

    Init commit - Added Blink LED

---
+++ Blink-LED/Blink-LED.ino
@@ -0,0 +1,14 @@
+#define led_gpio 32
+
+void setup() {
+  pinMode(led_gpio, OUTPUT);
+
+}
+
+void loop() {
+  digitalWrite(led_gpio, HIGH);
+  delay(1000);
+  digitalWrite(led_gpio, LOW);
+  delay(1000);
+
+}
